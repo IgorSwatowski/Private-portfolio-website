abdb639a290eae9cc76abbe1afc1f596702b0db4
Igor Swatowski <igi66@wp.pl>
2020-07-10 15:13:55 +0200

    Click anchor link and close the menu

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -4,6 +4,17 @@ const nav = document.querySelector('.navigation');
 const handleClick = () => {
   hamburger.classList.toggle('hamburger--active');
   nav.classList.toggle('navigation--active');
+  
+  $('.navigation a').on('click', function(){
+    $(".navigation").hide('navigation--active');
+    $(".hamburger").removeClass("hamburger--active");
+});
+
+$('.hamburger').on('click', function(){
+  $(".navigation").show('navigation--active');
+  $(".hamburger").addClass("hamburger--active");
+});
+
 }
 
 hamburger.addEventListener('click', handleClick);
